41e107bdcd4339ae4677eb4c4efb4ad60f86ae4b
unknown <Luthfi.adnan@gmail.com>
2023-03-20 21:04:10 +0700

    Belajar kondisi elif

--- 4. Conditional Statements.py
+++ 4. Conditional Statements.py
@@ -1,6 +1,7 @@
 x = 5
 y = 10
 z = 15
+a = 10
 
 # Conditional Statement 
 print(not(x > y))
@@ -13,7 +14,14 @@ if x == y: #False
 if x != y: #True
     print("X tidak sama dengan Y")
 
-if x <= y:
-    print("X lebih kecil dari Y")
+if (x < y):
+    print("X lebih kecil dari Y") #Ketika kondisinya True
 else:
     print("X lebih besar dari Y")
+
+if x > y:
+    print("X lebih kecil dari Y")
+elif y == a:
+    print("Y sama dengan Z")
+else:
+    print("Tidak ada kondisi True")
