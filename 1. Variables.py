ac2654eac47997f272b38f93b8a3d110505124cd
unknown <Luthfi.adnan@gmail.com>
2023-03-20 09:58:37 +0700

    Belajar Variable menggunakan Python

---
+++ 1. Variables.py
@@ -0,0 +1,15 @@
+angka = 5
+angka_berkoma = 3.8
+nama = input("Nama Lengkap: ")
+
+
+print(str(angka) + " " + str(angka_berkoma))
+print("Selamat Datang "+ nama + "!")
+
+angka_int = 5
+angka_str = str(angka_int)
+angka_float = float(angka_str)
+
+print(type(angka_int))
+print(type(angka_str))
+print(type(angka_float))
