70f2538fbc6527a2be0d548d4352a3e8137e740d
unknown <Luthfi.adnan@gmail.com>
2023-03-20 10:14:08 +0700

    Belajar Aritmatika manggunakan Python

---
+++ 2. Operator Aritmatic.py
@@ -0,0 +1,21 @@
+angka1 = 5
+angka2 = 3
+
+# print("Hasil Penjumlahan: " + str(angka1 + angka2))
+print(angka1%angka2)
+print(2**4)
+
+angka = 5
+print("Angka sebelum ditambah: " + str(angka))
+
+angka += 5
+print("Angka setelah ditambah: " + str(angka))
+
+import math
+
+print(math.pow(2, 3)) #Untuk melakukan pangkat
+print(math.ceil(3.2)) #Untuk melakukan pembulatan keatas
+print(math.floor(3.2)) #Untuk melakukan pembulatan kebawah
+print(math.sqrt(25)) #Untuk melakukan bilangan akar
+
+print(math.pi) #Bilangan Phi
