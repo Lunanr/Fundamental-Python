5696de9f11f6171375e595d42d2675952a6eff61
unknown <Luthfi.adnan@gmail.com>
2023-03-20 10:36:26 +0700

    Belajar String dengan Python

--- 2. Operator Aritmatic.py
+++ 2. Operator Aritmatic.py
@@ -1,7 +1,7 @@
 angka1 = 5
 angka2 = 3
 
-# print("Hasil Penjumlahan: " + str(angka1 + angka2))
+print("Hasil Penjumlahan: " + str(angka1 + angka2))
 print(angka1%angka2)
 print(2**4)
 
---
+++ 3. String.py
@@ -0,0 +1,20 @@
+text = "luthfi Adnan r"
+text1 = "seorang data Scientist"
+umur = 20
+
+#String Methods
+print(text.upper()) #Membuat kalimat menjadi huruf kapital semua
+print(text.lower()) #Membuat kalimat menjadi huruf kecil
+print(len(text)) #Menghitung berapa banyak character
+print(text.split(' ')) #Memotong berdasarkan spasi
+print(text.capitalize()) #Merubah huruf depan menjadi besar
+print(text.index('a')) #Mencari index dalam sebuah karater
+print(text[-1]) #Mencetak karater dari -1
+print(text[:]) #Mecetak dari karater pertama hingga akhir
+print(text + " " + text1 + " ")
+
+text2 = "{} berumur {} tahun sebagai {}"
+print(text2.format(text,umur,text1))
+
+print("tahun" in text2)
+print("Umur" in text2)
