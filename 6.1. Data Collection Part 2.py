b9938427fb8bd3e3c3645ee3434ec72774aa6300
unknown <Luthfi.adnan@gmail.com>
2023-03-24 10:08:29 +0700

    Belajar Function create and call, lambda Function

--- 6.1. Data Collection Part 2.py
+++ 6.1. Data Collection Part 2.py
@@ -1,30 +1,33 @@
 #Tuple
-# tupleExample = (2, 3, 'python', 3.8, True) #Perbedaan dengan list dari kurungnya
-# # Tuple tidak bisa merubah isi dari valuenya
-# print(tupleExample[0:3])
+tupleExample = (2, 3, 'python', 3.8, True) #Perbedaan dengan list dari kurungnya
+# Tuple tidak bisa merubah isi dari valuenya
+print(tupleExample[0:3])
 
 #Dictionary
 """Kamus yang digunakan untuk menyimpan data dalam bentuk key:value dan
 menggunakan kurung kurawal """
-# customer = {
-#     "id" : 100,
-#     "nama" : "Luthfi",
-#     "Umur" : 22
-# }
+customer = {
+    "id" : 100,
+    "nama" : "Luthfi",
+    "Umur" : 22
+}
 
-# customer["pekerjaan"] = "pengusaha" #Menambahkan nilai dan value
-# # customer.pop("umur") #Untuk menghilangkan value
+customer["pekerjaan"] = "pengusaha" #Menambahkan nilai dan value
+customer.pop("umur") #Untuk menghilangkan value
 
-# print(customer)
-# # print(customer["nama"])
-# # print(customer["Umur"])
+print(customer)
+print(customer["nama"])
+print(customer["Umur"])
 
 #Set
 numbers1 = {1, 3, 5, 7, 10}
 numbers2 = {3, 4, 6, 8, 10}
 
-# numbers_union = numbers1.union(numbers2) #Menggabungkan dua nilai dan angka duplikat muncul sekali
-# numbers_different = numbers1.difference(numbers2) #Mencari nilai yang berbeda dari numbers1
-# numbers_symetric = numbers1.symmetric_difference(numbers2) #Mencetak semua nilai yang berbeda
+numbers_union = numbers1.union(numbers2) #Menggabungkan dua nilai dan angka duplikat muncul sekali
+numbers_different = numbers1.difference(numbers2) #Mencari nilai yang berbeda dari numbers1
+numbers_symetric = numbers1.symmetric_difference(numbers2) #Mencetak semua nilai yang berbeda
 numbers_intersection = numbers1.intersection(numbers2) #Mencetak nilai yang sama
+print(numbers_union)
+print(numbers_different)
+print(numbers_symetric)
 print(numbers_intersection)
---
+++ 7. Functions.py
@@ -0,0 +1,18 @@
+#Function create and call
+# def greet(name,age):
+#     print("Hello, " + name)
+#     print("Age: " + str(age))
+
+# greet("Lunan",20)
+# greet("Rahman",22)
+# greet(age = 30, name = "cecep")
+
+# def add5(x):
+#     total = x + 5
+#     return total
+
+# print(add5(2))
+
+#Lambda Function Untuk membuat sebuah function menjadi singkat
+add4 = lambda n: n+5
+print(add4(4))
