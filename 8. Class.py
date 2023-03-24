df0f7c1a268e4aba63f2098b9536e59688bf6bec
unknown <Luthfi.adnan@gmail.com>
2023-03-24 11:08:46 +0700

    Belajar Parent Class dan Child Class

--- 8. Class.py
+++ 8. Class.py
@@ -15,12 +15,31 @@ class x:
         self.age = age
         self.pekerjaan = pekerjaan
 
+    def greet(self):
+        print("Halo, " + self.name + "!")
+
 p1 = x("Lunan", 32, "Data Science")
 p3 = x("Abiyyu", 15, "Pelajar")
 
-print(p1.name)
-print(p1.age)
-print(p1.pekerjaan)
-print(p3.name)
-print(p3.age)
-print(p3.pekerjaan)    
+# print(p1.__dict__)
+# print(p3.__dict__)    
+p1.greet()
+
+class Binatang: #Class yang lebih general / Parent Class
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Cat(Binatang): #Child Class
+    def __init__(self, name, age, color, weight):
+        super().__init__(name,age)
+        self.color = color
+        self.weight = weight
+
+class Dog(Binatang): #Child Class
+    def __init__(self, name, age, type):
+        super().__init__(name,age)
+        self.type = type
+
+a = Dog("Herder", 15, "Mamals")
+print(a.__dict__)
